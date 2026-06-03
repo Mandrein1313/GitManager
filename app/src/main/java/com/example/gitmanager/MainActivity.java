63fe63bfd8cb0093bf4971aa1d26642fd9ebd287
root <root@localhost>
2026-06-03 15:57:00 +0700

    Cloud Build Request - 2026-06-03 15:57:00

--- app/src/main/java/com/example/gitmanager/MainActivity.java
+++ app/src/main/java/com/example/gitmanager/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Bundleข}จ;
+import android.os.Bundle};
 import android.os.Environment;
 import android.provider.Settings;
 import android.widget.*;
